b10bc48f15d0ac1b979663a161d08acfdb6a144a
mattsfowler <mattsfwlr@gmail.com>
2022-05-14 14:43:51 +0100

    Pre Game Engine Purge

---
+++ LeviathanEngine/LeviathanEngine/HexMap/HexTile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeviathanEngine
+{
+    public struct HexTile
+    {
+        public string terrain;
+        public int population;
+        public string tradeGood;
+        public bool isCapitol;
+    }
+}
---
+++ LeviathanEngine/LeviathanEngineTests/HexMapTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using LeviathanEngine;
+using LeviathanEngine.HexMap;
+using LeviathanEngine.HexMap.Coords;
+
+namespace LeviathanEngineTests
+{
+    [TestClass]
+    public class HexMapTests
+    {
+        [TestMethod]
+        public void CanGetTerrainOfTile()
+        {
+            HexMap hexMap = new HexMap(5, 5);
+            var terrainVal = "test";
+
+            hexMap.SetTerrain(2, 2, terrainVal);
+            Assert.AreEqual(terrainVal, hexMap.GetTerrain(2, 2));
+            Assert.AreNotEqual(terrainVal, hexMap.GetTerrain(1, 1));
+        }
+    }
+}
